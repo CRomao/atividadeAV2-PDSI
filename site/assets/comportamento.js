5b13c5091764a28aa93fb6216291f689b4d6bf61
CRomao <ciceroromao1923@gmail.com>
2019-11-15 22:50:43 -0300

    upload files

---
+++ site/assets/comportamento.js
@@ -0,0 +1,5 @@
+var btnMenu = document.querySelector(".menu-hamburguer");
+
+btnMenu.addEventListener("click", function() {
+	console.log("Esse método será chamado quando um clique for dado no elemento com a classe .menu-hamburguer");
+});
